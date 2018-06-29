f2c1484812c8b8f2b8d39719ba1c347fddb6fcc0
RiCK BROWN <ricky.brown.00@gmail.com>
2018-06-29 16:08:48 +0100

    Backend API working and tested with POSTMAN

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
+const items = require('./routes/api/items')
+
 const app = express()
 
 // body-parser middleware
@@ -15,6 +17,9 @@ mongoose
   .connect(db)
   .then(() => console.log('mongoDB connected..!'))
   .catch(error => console.log(error))
+
+// use routes
+app.use('/api/items', items)
 
 // set up port to 'listen' 
 const port = process.env.PORT || 5000
