30ffbb7c3cca618342e17e71fee97486e38c5e14
RiCK BROWN <ricky.brown.00@gmail.com>
2018-07-01 19:39:55 +0100

    Redux store setup

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,18 @@ import './App.css'
 import AppNavBar from './components/AppNavBar'
 import ShoppingList from './components/ShoppingList'
 
+import { Provider } from 'react-redux'
+import store from './store'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <AppNavBar />
-        <ShoppingList />
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <AppNavBar />
+          <ShoppingList />
+        </div>
+      </Provider>
     )
   }
 }
