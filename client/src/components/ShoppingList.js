1cb43782333a62c2cd3190f6015ce69e317a3e61
RiCK BROWN <ricky.brown.00@gmail.com>
2018-07-01 20:36:07 +0100

    itemModal created

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,30 +1,21 @@
 import React, { Component } from 'react'
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
-import uuid from 'uuid'
 import { connect } from 'react-redux'
-import { getItems } from '../actions/itemActions'
+import { getItems, deleteItem } from '../actions/itemActions'
 import PropTypes from 'prop-types'
 
 class ShoppingList extends Component {
   componentDidMount() {
     this.props.getItems()  
   }
+
+  onDeleteClick = id => this.props.deleteItem(id)
   
   render() {
     const { items } = this.props.item
     return(
       <Container>
-        <Button color="dark"
-                style={{ marginBottom: '2rem' }}
-                onClick={() => {
-                  const name = prompt('Enter item')
-                  if(name) {
-                    this.setState(state => ({
-                      items: [...state.items, { id: uuid(), name }]
-                    }))
-                  }
-                }}>Add Item</Button>
         <ListGroup>
           <TransitionGroup className="shopping-list">
             {
@@ -36,11 +27,7 @@ class ShoppingList extends Component {
                     <Button className="remove-btn"
                             color="danger"
                             size="sm"
-                            onClick={() => {
-                              this.setState(state => ({
-                                items: state.items.filter(item => item.id !== id)
-                              }))
-                            }}>&times;
+                            onClick={this.onDeleteClick.bind(this, id)}>&times;
                     </Button>
                     {
                       name
@@ -65,4 +52,4 @@ const mapStateToProps = state => ({
   item: state.item
 })
 
-export default connect(mapStateToProps, { getItems })(ShoppingList)
+export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList)
--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -1,5 +1,5 @@
 import uuid from 'uuid'
-import { GET_ITEMS, ADD_ITEMS, DELETE_ITEMS } from '../actions/types'
+import { GET_ITEMS, ADD_ITEMS, DELETE_ITEM } from '../actions/types'
 
 const initialState ={
   items: [
@@ -16,7 +16,19 @@ export default function(state = initialState, action) {
       return {
         ...state
       }
-      
+
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter(item => item.id !== action.payload)
+      }
+
+    case ADD_ITEMS:
+      return{
+        ...state,
+        items: [action.payload, ...state.items]
+      }
+
     default: 
       return state
   }
